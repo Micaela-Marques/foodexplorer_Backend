96ade5e7d6545e3e6bda2a97e21f81dc93fbd8f9
Micaela Marques <micaellagrangeiro@gmail.com>
2024-09-10 16:30:36 -0300

    I created the password and email checker in sessions controller

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express')
 
 const usersRouter = require('./users.routes')
 const productRouter = require('./createProduct.routes')
+const sessionsRouter = require('./sessions.routes')
 
 const routes = Router()
 
 routes.use('/users', usersRouter);
 routes.use('/product', productRouter);
+routes.use('/sessions', sessionsRouter);
 
 module.exports = routes;
