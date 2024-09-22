3a6a71981609b843924923446c39bc6e9b04df06
Micaela Marques <micaellagrangeiro@gmail.com>
2024-09-22 16:39:03 -0300

    ajusted route for category

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,12 +2,14 @@ const { Router } = require("express");
 
 const usersRouter = require("./users.routes");
 const productRouter = require("./createProduct.routes");
+const categoryRouter = require("./createCategory.routes");
 const sessionsRouter = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
 routes.use("/product", productRouter);
+routes.use("/categories", categoryRouter);
 routes.use("/sessions", sessionsRouter);
 
 module.exports = routes;
