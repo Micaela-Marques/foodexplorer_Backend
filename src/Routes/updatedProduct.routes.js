839485737069cbf81f690f223929e7f313e41987
Micaela Marques <micaellagrangeiro@gmail.com>
2024-09-27 01:00:31 -0300

    creation router updated

---
+++ src/Routes/updatedProduct.routes.js
@@ -0,0 +1,29 @@
+const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
+const ProductController = require("../controllers/productController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
+const productRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
+const productController = new ProductController();
+
+// Middleware de autenticação aplicado a todas as rotas
+productRoutes.use(ensureAuthenticated);
+
+// Rota para atualizar produto (incluindo a imagem)
+productRoutes.put('/:id', upload.single('image'), productController.updatedProduct);
+
+// Rota para listar produtos (assumindo que existe um método listProducts no controller)
+productRoutes.get('/', productController.listProducts);
+
+// Rota para obter um produto específico por ID
+productRoutes.get('/:id', productController.show);
+
+// Rota para obter informações do carrinho
+productRoutes.get('/cart/:id', productController.showCart);
+
+// Rota para obter informações do carrinho para edição
+productRoutes.get('/admin/new/:id', productController.showCartEdit);
+
+module.exports = productRoutes;
